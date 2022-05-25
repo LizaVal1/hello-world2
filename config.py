a92d288739e6570f66e515c7919bec6cf4a932d1
Elizabeth Valenchits <101469856+LizaVal1@users.noreply.github.com>
2022-05-25 22:04:00 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = "5309183789:AAEdUAaQ2dTCQ9PCRsIXdqcaGqZjaoqpehA"
+
+keys={
+    'рубль': 'RUB',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'белруб': 'BYN',
+    'фунт': 'GBP',
+    'биткоин': 'BTC',
+    'эфириум': 'ETH'
+     }
